a579fd39ba3953611f802276d76a85109ad48f75
ryebr3ad <rprauschenberg@gmail.com>
2021-01-09 19:52:36 -0500

    Add quick sort to sort service

--- src/app/services/sort/sort.service.spec.ts
+++ src/app/services/sort/sort.service.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { doesNotReject } from 'assert';
 import { Observable, Subject } from 'rxjs';
 
 import { SortService, InsertionSortView, MergeSortView } from './sort.service';
 
 describe('SortService', () => {
   let service: SortService;
+
+  let sequence: number[];
+
+  beforeEach(() => {
+    sequence = [];
+    for (let i = 10; i > 0; i--) {
+      sequence.push(i);
+    }
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -16,15 +26,6 @@ describe('SortService', () => {
   });
 
   describe('insertion sort', () => {
-
-    let sequence: number[];
-
-    beforeEach(() => {
-      sequence = [];
-      for (let i = 100; i > 0; i--) {
-        sequence.push(i);
-      }
-    });
 
     it('should return a number of iterations equal to the length of the array', (done: DoneFn) => {
       service.insertionSort(sequence).subscribe((iters: InsertionSortView) => {
@@ -47,22 +48,24 @@ describe('SortService', () => {
 
   describe('merge sort', () => {
 
-    let sequence: number[];
-
-    beforeEach(() => {
-      sequence = [];
-      for (let i = 10; i > 0; i--) {
-        sequence.push(i);
-      }
-    });
-
-    it('should sort the array in increasing order and stored in the first element of the array of iterations', (done: DoneFn) => {
+    it('should sort the array in increasing order and be stored in the first element of the array of iterations', (done: DoneFn) => {
       service.mergeSort(sequence).subscribe((iters: MergeSortView) => {
         let sorted: number[] = iters[iters.length-1][0].left;
         for(let i = 0; i < sorted.length-1; i++) {
           expect(sorted[i]).toBeLessThanOrEqual(sorted[i+1]);
         }
         done();
+      });
+    });
+  });
+
+  describe('quick sort', () => {
+    it('should sort the array in increasing order', (done: DoneFn) => {
+      service.quickSort(sequence).subscribe((sorted) => {
+        for(let i = 0; i < sorted.length-1; i++) {
+          expect(sorted[i]).toBeLessThanOrEqual(sorted[i+1]);
+        }
+        done()
       });
     })
   })
--- src/app/services/sort/sort.service.ts
+++ src/app/services/sort/sort.service.ts
@@ -48,6 +48,41 @@ export class SortService {
     return of(iters);
   }
 
+  quickSort(sequence: number[]): Observable<number[]>{
+    let clonedSequence: number[] = Object.assign([], sequence);
+    this.shuffleArray(clonedSequence);
+    this.qsort(clonedSequence, 0, clonedSequence.length-1);
+    return of(clonedSequence);
+  }
+
+  private shuffleArray(sequence: number[]){
+    for (var i = sequence.length - 1; i > 0; i--) {
+        var rand = Math.floor(Math.random() * (i + 1));
+        [sequence[i], sequence[rand]] = [sequence[rand], sequence[i]]
+    }
+}
+
+  private qsort(sequence: number[], p: number, r: number) {
+    if(p < r) {
+      let q = this.partition(sequence, p, r);
+      this.qsort(sequence, p, q - 1);
+      this.qsort(sequence, q + 1, r);
+    }
+  }
+
+  private partition(sequence: number[], p: number, r: number) {
+    let x: number = sequence[r];
+    let i = p - 1;
+    for(let j = p; j <= r - 1; j++) {
+      if(sequence[j] <= x) {
+        i++;
+        [sequence[i], sequence[j]] = [sequence[j], sequence[i]];
+      }
+    }
+    [sequence[i+1], sequence[r]] = [sequence[r], sequence[i+1]];
+    return i+1;
+  }
+
   private merge(iterations: MergeSortView, sequence: number[], lower: number, middle: number, upper: number, level: number): void {
 
     //Break case -- arrays of size one or less do not need to be divided any further
